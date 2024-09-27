e16739e1d9787f955539fa250276121d5bc47f3c
ViviFer933 <vfernand@ine.gub.uy>
2024-09-26 21:10:22 -0300

    Agrego ejerciciob_VF

--- Ejerciciob_VF.py
+++ Ejerciciob_VF.py
@@ -11,17 +11,59 @@ ar ='demo_data.csv'
 ar = pd.read_csv(ar)
 
 st.title ('Mi dashboard VF. Ejercicio b')
-st.write(ar)
 
-tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+# Agrupar y sumar la columna 'Cantidad' por 'Mes' y 'Producto'
+ar_prod = ar.groupby(['Mes', 'Producto'], as_index=False)['Cantidad'].sum()
+# Renombrar la columna de suma para mayor claridad
+ar_prod.rename(columns={'Cantidad': 'Cant_mes_prod'}, inplace=True)
 
+# Agrupar y sumar la columna 'Cantidad' por 'Mes' y 'Producto'
+ar_region = ar.groupby(['Mes', 'Region'], as_index=False)['Cantidad'].sum()
+
+# Renombrar la columna de suma para mayor claridad
+ar_region.rename(columns={'Cantidad': 'Cant_mes_reg'}, inplace=True)
+
+#Creo una barra lateral para el filtro
+st.sidebar.header('Filtros')
+
+
+# Creo las tabs
+tab1, tab2, tab3 = st.tabs(['Base de datos', '📈 Gráfico por mes', '📈 Región'])
+
+# Pestaña 1: Base de datos
 with tab1:
-    st.header("Productos")
-    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-with tab2:
-    st.header("Mes")
-    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-with tab3:
-    st.header("Region")
-    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+    st.subheader('Base de datos')
+    st.write(ar)
 
+# Pestaña 2: Gráfico por mes
+with tab2:
+    st.subheader('📈 Gráfico por mes')
+    
+    # Aplicar filtros
+    filtro_mes = st.sidebar.multiselect('Filtra por Mes:', ar_prod['Mes'].unique())
+    filtro_prod = st.sidebar.multiselect('Filtra por Producto:', ar_prod['Producto'].unique())
+    
+    # Filtrar datos
+    if filtro_mes:
+        ar_prod = ar_prod[ar_prod['Mes'].isin(filtro_mes)]
+    if filtro_prod:
+        ar_prod = ar_prod[ar_prod['Producto'].isin(filtro_prod)]
+    
+    # Crear gráfico
+    fig2 = px.bar(ar_prod, x='Producto', y='Cant_mes_prod', color='Mes', title='Cantidad vendida por mes y producto')
+    st.plotly_chart(fig2)
+
+# Pestaña 3: Gráfico por región
+with tab3:
+    st.header('📈 Región')
+    
+    # Aplicar filtros
+    if filtro_mes:
+        ar_region = ar_region[ar_region['Mes'].isin(filtro_mes)]
+    if filtro_prod:
+        ar_region = ar_region[ar_region['Region'].isin(filtro_prod)]
+    
+    # Crear gráfico
+    fig3 = px.line(ar_region, x='Region', y='Cant_mes_reg', color='Mes', title='Cantidad vendida por mes y región')
+    st.plotly_chart(fig3)
+    
